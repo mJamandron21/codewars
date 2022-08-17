077a0960253af1828dffe7a901a584d43cb1ccad
Mark Kenneth Jamandron <mjamandron@gmail.com>
2022-08-17 22:37:52 +0800

    Create simple_join.sql

---
+++ sql/simple_join.sql
@@ -0,0 +1,9 @@
+SELECT    p.id
+        , p.name
+        , p.isbn
+        , p.company_id
+        , p.price
+        , c.name AS company_name
+FROM products AS p
+LEFT JOIN companies AS c
+ON p.company_id = c.id
