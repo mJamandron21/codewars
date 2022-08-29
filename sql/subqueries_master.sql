072ec6d50a0c90d0aa4d1da07d9337238b5c47bb
Mark Kenneth Jamandron <mjamandron@gmail.com>
2022-08-29 20:48:02 +0800

    Create subqueries_master.sql

---
+++ sql/subqueries_master.sql
@@ -0,0 +1,13 @@
+select 
+(case when sub_query.test='' then sub_query.name else concat(sub_query.name,
+' ', sub_query.first_lastname) end) as name,
+(case when sub_query.test='' then sub_query.first_lastname else 
+ sub_query.second_lastname end) as first_lastname,
+ (case when sub_query.test='' then sub_query.second_lastname else
+ test end) as second_lastname
+ from
+ (select split_part(name, ' ', 1) as name, 
+         split_part(name, ' ', 2) as first_lastname,
+         split_part(name, ' ', 3) as second_lastname,
+         split_part(name, ' ', 4) as test
+         from people) as sub_query;
